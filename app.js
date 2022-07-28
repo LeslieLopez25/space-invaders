01509239ecbf7e74f1a0ef4a5b30030bbbeaae03
Leslie Lopez <leslie2515lopez@gmail.com>
2022-07-27 22:03:11 -0500

    Added move shooter function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector(".grid");
 let currentShooterIndex = 202;
+let width = 15;
 
 for (let i = 0; i < 225; i++) {
   const square = document.createElement("div");
@@ -22,3 +23,17 @@ function draw() {
 draw();
 
 squares[currentShooterIndex].classList.add("shooter");
+
+function moveShooter(e) {
+  squares[currentShooterIndex].classList.remove("shooter");
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentShooterIndex % width !== 0) currentShooterIndex -= 1;
+      break;
+    case "ArrowRight":
+      if (currentShooterIndex % width < width - 1) currentShooterIndex += 1;
+      break;
+  }
+  squares[currentShooterIndex].classList.add("shooter");
+}
+document.addEventListener("keydown", moveShooter);
